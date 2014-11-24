6a799236dfbabf1a36ca9feb6b2e495bd8d40279
wowditi <niels_geuze@hotmail.com>
2014-11-24 15:02:16 +0100

    parser

---
+++ src/ParseText.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ParseText {
+	public static List<String[]> parseString(List<String> set) {
+		List<String[]> returnset = new ArrayList<String[]>();
+		for (int i = 0; i < set.size(); i++) {
+			
+			String[] words = set.get(i).replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+			returnset.add(words);
+		}
+		return returnset;
+		
+	}
+	public static void main (String[] args) {
+		List<String> test = new ArrayList<String>();
+		test.add("hoi Werkt, dit??? xD\t \r \n nnooo sdfsdfd;sa da[]?");
+		List<String[]> tester = parseString(test);
+		for (int i = 0; i < 7; i++) {
+			System.out.println(tester.get(0)[i]);
+		}
+	}
+}
